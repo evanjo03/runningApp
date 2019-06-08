8225cde059b4cc7a067c7f1b3fe14a88ce44c636
John Evans <evanjo03@luther.edu>
2019-06-08 13:50:34 -0500

    added routing

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,21 +5,30 @@ import { Link } from "react-router-dom"
 
 function Navbar() {
     return (
-        <ul >
-            <li>
-                <Link to="/">Home</Link>
+        <ul className="nav nav-tabs">
+            <li className="nav-item">
+                <Link to="/" className={window.location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                    Home
+        </Link>
             </li>
-            <li>
-                <Link to="/about/">About</Link>
+            <li className="nav-item">
+                <Link
+                    to="/log"
+                    className={window.location.pathname === "/log" ? "nav-link active" : "nav-link"}
+                >
+                    Log Run
+        </Link>
             </li>
-            <li>
-                <Link to="/users/">Users</Link>
+            <li className="nav-item">
+                <Link
+                    to="/list"
+                    className={window.location.pathname === "/list" ? "nav-link active" : "nav-link"}
+                >My Runs
+        </Link>
             </li>
         </ul>
-        
-    )
+    );
 }
 
 export default Navbar;
-
 
