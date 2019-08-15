64d7539f45e931a70e75c6a52fb8b88094e39639
John Evans <john.wes.evans@gmail.com>
2019-08-15 16:50:36 -0500

    prelim on navbar

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,6 @@ import List from "./components/Pages/List";
 import Nav from "./components/Nav"
 import SignIn from "./components/Pages/SignIn";
 import Footer from "./components/Footer"
-import Navbar from "./components/Navbar";
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
@@ -15,8 +14,7 @@ function App() {
     <div>
       <Router>
         <div>
-          <Nav>Hi</Nav>
-          <Navbar/>
+          <Nav/>
           <Route exact path="/" component={Home} />
           <Route exact path="/log" component={Log} />
           <Route exact path="/list" component={List} />
--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,18 +1,51 @@
 import React from 'react'
 import AppBar from '@material-ui/core/AppBar'
+import { Link } from "react-router-dom"
+// import Link from '@material-ui/core/Link';
 import Toolbar from '@material-ui/core/Toolbar'
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles(theme => ({
+    link: {
+      margin: theme.spacing(1),
+    },
+  }));
+
+
 const Nav = () => {
+    const classes = useStyles();
+    let isLoggedIn = window.localStorage.getItem("username"), logout = () => {
+        localStorage.clear();
+        window.location.reload();
+    }
+    if (isLoggedIn) {
     return(
         <div>
         <AppBar position="static">
             <Toolbar>
-                <Typography variant="title" color="inherit">
-                React & Material-UI Sample Application
+                <Typography variant="h6" color="inherit">
+                {/* <ul className="nav nav-tabs"> */}
+                    
+                        <Link
+                            to="/log"
+                            className={classes.link}>
+                        Log Run
+                        </Link>
+                    
+                    
+                        <Link
+                            to="/list"
+                            className={classes.link}
+                        >My Runs
+                        </Link>
+                    
+                    {(isLoggedIn ? <button onClick={logout}>Logout</button> : null)}
+                    {/* </ul> */}
                 </Typography>
             </Toolbar>
         </AppBar>
         </div>
-    )
+    )} else { return null }
 }
 export default Nav;
