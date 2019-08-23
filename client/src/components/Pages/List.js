0c5cc7e57ac3f075a51c29001fd108ca46658174
John Evans <john.wes.evans@gmail.com>
2019-08-23 15:06:16 -0500

    about done with the list page, fixed hooks, fixed looped useEffect issue

--- client/src/components/Pages/List.js
+++ client/src/components/Pages/List.js
@@ -1,87 +1,65 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { Redirect } from 'react-router-dom';
 import API from "../../utils/API"
 import Run from "../Run";
-import { CardBody, Card , Row, Col } from "reactstrap"
+import { CardBody, Card, Row, Col } from "reactstrap"
 
+const List = () => {
+    const [username, setUsername] = useState("");
+    const [results, setResults] = useState([]);
+    const [redirect, setRedirect] = useState(false);
 
-
-class List extends Component {
-    state = {
-        username: "",
-        results: []
-    }
-
-
-    setRedirect = () => {
-        this.setState({
-            redirect: true
-        })
-    }
-    renderRedirect = () => {
-        if (this.state.redirect) {
-            return <Redirect to='/' />
-        }
-    }
-
-    getRuns = (username) => {
-        API.getUser(username).then(results => {
+    
+    const getRuns = (usnme) => {
+        API.getUser(usnme).then(results => {
             console.log(results.data.activities)
-            this.setState({ "results": results.data.activities })
+            setResults(results.data.activities)
         })
     }
-    componentDidMount = () => {
-        let user = JSON.parse(localStorage.getItem("username"));
+    
+    useEffect(() => {
+        let user = JSON.parse(localStorage.getItem("username"))
         if (user) {
-            this.setState({ "username": user }, () => {
-                this.getRuns(this.state.username);
-            })
-        } else { this.setRedirect() }
+            setUsername(user)
+            getRuns(user)
+        } else { setRedirect() }
+    }, [])
 
 
-
+const renderRedirect = () => {
+    if (redirect) {
+        return <Redirect to='/' />
     }
-    render() {
-        return (
-            <div>
-                {this.renderRedirect()}
-                <Row>
-                    <Col>
-                        <Card className="runCard">
-                            <CardBody className="listCardWrapper">
-                                <h4>{this.state.username}'s Runs</h4>
-                                    {this.state.results.map(run => {
-                                        return (
-                                         
-                                            <Run
-                                            hours={run.hours}
-                                            minutes={run.minutes}
-                                            seconds={run.seconds}
-                                            distance={run.distance}
-                                            description={run.description}
-                                            key={run.id}
-                                            id = {run.id}
-                                        />
-                                        )
-
-                                        })}
-                            </CardBody>
-                        </Card>
-                    </Col>
-                </Row>
-            </div>
-        )
-    }
-
-
-
-
-
-
-
-
-
-
 }
 
+return (
+    <div>
+        {renderRedirect()}
+        <Row>
+            <Col>
+                <Card className="runCard">
+                    <CardBody className="listCardWrapper">
+                        <h4>{username}'s Runs</h4>
+                        {results.map(run => {
+                            return (
+
+                                <Run
+                                    hours={run.hours}
+                                    minutes={run.minutes}
+                                    seconds={run.seconds}
+                                    distance={run.distance}
+                                    description={run.description}
+                                    key={run.id}
+                                    id={run.id}
+                                />
+                            )
+
+                        })}
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+    </div>
+)
+}
 export default List;
