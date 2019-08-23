9f56d28f60a7582568dfa3b8ac1fe3a353289651
John Evans <john.wes.evans@gmail.com>
2019-08-23 14:31:15 -0500

    Home.js page fully migrated to hooks from setState whoooooooo

--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -1,93 +1,76 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 import { Link, Redirect } from 'react-router-dom';
 import API from "../../utils/API"
-import { Form, FormGroup, Label, Input, CardBody, Card , Row, Col } from "reactstrap"
+import { Form, FormGroup, Label, Input, CardBody, Card, Row, Col } from "reactstrap"
 
+const Home = () => {
 
-class Home extends Component {
-    state = {
-        firstName: "",
-        lastName: "",
-        username: "",
-        password: "",
-        redirect: false
-    }
+    //hooky bois
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [redirect, setRedirect] = useState(false);
 
-    setRedirect = () => {
-        this.setState({
-            redirect: true
-        })
-    }
-    renderRedirect = () => {
-        if (this.state.redirect) {
-            return <Redirect to='/log' />
-        }
-    }
-
-    handleChange = event => {
-        const { name, value } = event.target;
-
-        this.setState({
-            [name]: value
-        })
-    }
-
-    handleSubmit = event => {
-        event.preventDefault();
-        console.log(this.state);
-        API.getUser(this.state.username).then(result => {
+    const handleSubmit = e => {
+        e.preventDefault();
+        API.getUser(username).then(result => {
             (result.data ? alert("User already exists") :
                 API.createUser({
-                    firstName: this.state.firstName,
-                    lastName: this.state.lastName,
-                    username: this.state.username,
-                    password: this.state.password
+                    firstName: firstName,
+                    lastName: lastName,
+                    username: username,
+                    password: password
                 })
                     .then(result => {
                         console.log(result.status)
                         localStorage.setItem("username", JSON.stringify(result.data.username));
-                        this.setRedirect();
+                        setRedirect(true);
                     })
             );
         })
     }
 
-    render() {
-        return (
-            <Row>
-                <Col sm="6" md="6" lg="6" className="mx-auto">
-                    <Card className="loginCard">
-                    <CardBody>
-                {this.renderRedirect()}
-                
-                <h1>New User</h1>
-                <h2>{this.state.name}</h2>
-                <Form onSubmit={this.handleSubmit}>
-                <FormGroup>
-                    <Label>First Name</Label>
-                    <Input type="text" name="firstName" value={this.state.firstName} onChange={this.handleChange} />
-                </FormGroup>
-                <FormGroup>
-                    <Label>Last Name</Label>
-                    <Input type="text" name="lastName" value={this.state.lastName} onChange={this.handleChange} />
-                </FormGroup>
-                <FormGroup>
-                    <Label>Username</Label>
-                    <Input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
-                </FormGroup>
-                <FormGroup>
-                    <Label>Password</Label>
-                    <Input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
-                </FormGroup>
-                    <Input type="submit" value="Submit" />
-                    <br></br>
-                    <Link to="/signin">Already signed up? Sign in</Link>
-                </Form>
-                </CardBody>
-                </Card>
-                </Col>
-                </Row>
-        )
+
+    const renderRedirect = () => {
+        if (redirect) {
+            return <Redirect to='/log' />
+        }
     }
+
+    return (
+        <Row>
+            <Col sm="6" md="6" lg="6" className="mx-auto">
+                <Card className="loginCard">
+                    <CardBody>
+                        {renderRedirect()}
+                        <h1>New User</h1>
+                        <h2>{firstName}</h2>
+                        <Form onSubmit={handleSubmit}>
+                            <FormGroup>
+                                <Label>First Name</Label>
+                                <Input type="text" name="firstName" value={firstName} onChange={e => setFirstName(e.target.value)} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Last Name</Label>
+                                <Input type="text" name="lastName" value={lastName} onChange={e => setLastName(e.target.value)} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Username</Label>
+                                <Input type="text" name="username" value={username} onChange={e => setUsername(e.target.value)} />
+                            </FormGroup>
+                            <FormGroup>
+                                <Label>Password</Label>
+                                <Input type="password" name="password" value={password} onChange={e => setPassword(e.target.value)} />
+                            </FormGroup>
+                            <Input type="submit" value="Submit" />
+                            <br></br>
+                            <Link to="/signin">Already signed up? Sign in</Link>
+                        </Form>
+                    </CardBody>
+                </Card>
+            </Col>
+        </Row>
+    )
 }
 export default Home;
