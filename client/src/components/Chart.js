13530e157ae91d43c15507a1398bc4b4c1f26577
John Evans <john.wes.evans@gmail.com>
2019-08-26 11:06:51 -0500

    added graph to page

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,143 +1,69 @@
 import { ResponsiveLine } from '@nivo/line';
-import { useState, useEffect } from "react";
-import { Redirect } from 'react-router-dom';
-import API from "../utils/API"
 import React from "react";
-import Data from "./data";
-
-const Chart = () => {
-
-    const [username, setUsername] = useState("");
-    const [results, setResults] = useState([]);
-    const [redirect, setRedirect] = useState(false);
 
 
-    const getRuns = (usnme) => {
-        API.getUser(usnme).then(results => {
-            console.log(results.data.activities)
-            setResults(results.data.activities)
-        })
-    }
 
-    useEffect(() => {
-        let user = JSON.parse(localStorage.getItem("username"))
-        if (user) {
-            setUsername(user)
-            getRuns(user)
-        } else { setRedirect() }
-    }, [])
-
-
-    const renderRedirect = () => {
-        if (redirect) {
-            return <Redirect to='/' />
-        }
-    }
-    const style = {
-        chart: {
-            width: "100%",
-            height: 600
-        }
-    }
-
-    //lets create an object!!!! :)
-
-    const newData = [
-        {
-            "id": "distance",
-            "color": "hsl(344, 70%, 50%)",
-            "data": []
-        }
-    ]
-
-    results.forEach(result => {
-        let yVal = result.distance;
-        
-        let time = result.id;
-
-        //time ago in milliseconds that run took place
-        let timeDiffFromNow = Date.now() - time; 
-        
-
-        //time on x will be time diff from now - milliseconds from a week ago
-        let xVal = (604800000 - timeDiffFromNow) / (1000 * 60 * 60 * 24);
-
-        let runObj = {
-            "x" : xVal,
-            "y" : yVal
-        }
-
-        //push data 
-        newData[0].data.push(runObj)
-    })
-
-    //how pushing to our data object will look
-    
-    console.log(newData)
-
-    return (
-        <div className="chart" style={style.chart}>
-            <ResponsiveLine
-                data={newData}
-                margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-                xScale={{ type: 'point' }}
-                yScale={{ type: 'linear', stacked: true, min: 'auto', max: 'auto' }}
-                axisTop={null}
-                axisRight={null}
-                axisBottom={{
-                    orient: 'bottom',
-                    tickSize: 5,
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    legend: 'transportation',
-                    legendOffset: 36,
-                    legendPosition: 'middle'
-                }}
-                axisLeft={{
-                    orient: 'left',
-                    tickSize: 5,
-                    tickPadding: 5,
-                    tickRotation: 0,
-                    legend: 'count',
-                    legendOffset: -40,
-                    legendPosition: 'middle'
-                }}
-                colors={{ scheme: 'nivo' }}
-                pointSize={10}
-                pointColor={{ theme: 'background' }}
-                pointBorderWidth={2}
-                pointBorderColor={{ from: 'serieColor' }}
-                pointLabel="y"
-                pointLabelYOffset={-12}
-                useMesh={true}
-                legends={[
+const Chart = ({ data }) => (
+    <ResponsiveLine
+        data={data}
+        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+        xScale={{ type: 'point' }}
+        yScale={{ type: 'linear', stacked: true, min: 'auto', max: 'auto' }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+            orient: 'bottom',
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'Date',
+            legendOffset: 36,
+            legendPosition: 'middle'
+        }}
+        axisLeft={{
+            orient: 'left',
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'Miles',
+            legendOffset: -40,
+            legendPosition: 'middle'
+        }}
+        colors={{ scheme: 'nivo' }}
+        pointSize={10}
+        pointColor={{ theme: 'background' }}
+        pointBorderWidth={2}
+        pointBorderColor={{ from: 'serieColor' }}
+        pointLabel="y"
+        pointLabelYOffset={-12}
+        useMesh={true}
+        legends={[
+            {
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 100,
+                translateY: 0,
+                itemsSpacing: 0,
+                itemDirection: 'left-to-right',
+                itemWidth: 80,
+                itemHeight: 20,
+                itemOpacity: 0.75,
+                symbolSize: 12,
+                symbolShape: 'circle',
+                symbolBorderColor: 'rgba(0, 0, 0, .5)',
+                effects: [
                     {
-                        anchor: 'bottom-right',
-                        direction: 'column',
-                        justify: false,
-                        translateX: 100,
-                        translateY: 0,
-                        itemsSpacing: 0,
-                        itemDirection: 'left-to-right',
-                        itemWidth: 80,
-                        itemHeight: 20,
-                        itemOpacity: 0.75,
-                        symbolSize: 12,
-                        symbolShape: 'circle',
-                        symbolBorderColor: 'rgba(0, 0, 0, .5)',
-                        effects: [
-                            {
-                                on: 'hover',
-                                style: {
-                                    itemBackground: 'rgba(0, 0, 0, .03)',
-                                    itemOpacity: 1
-                                }
-                            }
-                        ]
+                        on: 'hover',
+                        style: {
+                            itemBackground: 'rgba(0, 0, 0, .03)',
+                            itemOpacity: 1
+                        }
                     }
-                ]}
-            />
-        </div>
-    )
-}
+                ]
+            }
+        ]}
+    />
+)
+
 export default Chart;
--- client/src/components/Pages/List.js
+++ client/src/components/Pages/List.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Redirect } from 'react-router-dom';
 import API from "../../utils/API"
 import Run from "../Run";
+import Chart from "../Chart"
 import { CardBody, Card, Row, Col } from "reactstrap"
 
 const List = () => {
@@ -31,18 +32,59 @@ const List = () => {
             return <Redirect to='/' />
         }
     }
+    const style = {
+        chart: {
+            width: "100%",
+            height: 600
+        }
+    }
+
+    //lets create an object!!!! :)
+
+    const newData = [
+        {
+            "id": "distance",
+            "color": "hsl(344, 70%, 50%)",
+            "data": []
+        }
+    ]
+
+    results.forEach(result => {
+        let yVal = result.distance;
+
+        let time = result.id;
+
+        //time ago in milliseconds that run took place
+        let timeDiffFromNow = Date.now() - time;
+
+
+        //time on x will be time diff from now - milliseconds from a week ago
+        let xVal = (604800000 - timeDiffFromNow) / (1000 * 60 * 60 * 24);
+
+        let runObj = {
+            "x": xVal,
+            "y": yVal
+        }
+
+        //push data 
+        newData[0].data.push(runObj)
+    })
+
 
     return (
         <div>
             {renderRedirect()}
             <Card className="runCard">
                 <CardBody className="listCardWrapper">
-                <h4>{username}'s Runs</h4>
+                    <h4>{username}'s Runs</h4>
                     <Row>
                         <Col sm={8}>
-                            <div>Graph</div>
+                            <div style={style.chart}>
+                                <Chart data={newData} />
+                            </div>
+
                         </Col>
-                        <Col sm = {4}>
+                        <Col sm={4}>
                             <div>Data</div>
                         </Col>
                     </Row>
